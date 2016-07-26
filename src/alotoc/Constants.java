8efe7855c720efbb7b6e9195d23cb05a1326bae7
Kazeheki <kaminski.andrea@web.de>
2016-07-26 17:14:24 +0200

    Implement Panels and Constants.

--- src/alotoc/Constants.java
+++ src/alotoc/Constants.java
@@ -1,5 +1,34 @@
 package alotoc;
 
+import java.awt.Color;
+
+/**
+ * This is a class only for constants needed in this project.
+ * 
+ * @author Kazeheki
+ *
+ */
 public final class Constants {
-	//TODO impl.
+	//sizes.
+	public static final int MAIN_FRM_HEIGHT = 100;
+	public static final int MAIN_FRM_WIDTH = 3 * MAIN_FRM_HEIGHT;
+	public static final int PREF_FRM_HEIGHT = 100;
+	public static final int PREF_FRM_WIDTH = 3 * PREF_FRM_HEIGHT;
+	public static final int SMALL_SIDE_PNL_SIZE = 15;
+	public static final int GO_BTN_SIZE = 50;
+	
+	//strings.
+	public static final String DOT_ON = ":";
+	public static final String DOT_OFF = " ";
+	
+	//time.
+	public static final long ONE_SEC_IN_MS = 1000;
+	
+	//color.
+	public static final Color[] COLORS = {
+			new Color(0, 0, 0),				//black.
+			new Color(51, 51, 51),			//grayish.
+			new Color(255, 255, 255),		//white.
+			new Color(255, 0, 0)			//red.
+	};
 }
--- src/alotoc/graphics/panels/QuitPanel.java
+++ src/alotoc/graphics/panels/QuitPanel.java
@@ -1,6 +1,28 @@
 package alotoc.graphics.panels;
 
+import java.awt.Color;
+
+import alotoc.Constants;
+import alotoc.graphics.view.AbstractAlotocFrame;
+
+/**
+ * This is a Panel for a Quit-Button.
+ * 
+ * @author Kazeheki
+ * @see Panel
+ */
 @SuppressWarnings("serial")
 class QuitPanel extends Panel {
-	//TODO impl.
+
+	/**
+	 * Ctor.
+	 * 
+	 * @param f Frame the panel shall be added to.
+	 */
+	QuitPanel(final AbstractAlotocFrame f) {
+		super(f.getWidth() - Constants.SMALL_SIDE_PNL_SIZE, 0, Constants.SMALL_SIDE_PNL_SIZE,
+				Constants.SMALL_SIDE_PNL_SIZE, "X");
+		this.setBackground(new Color(94, 04, 22, 128));
+		f.add(this);
+	}
 }
