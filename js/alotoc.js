4eb83a6d09a622f69ba7ae26baace94253d672fd
Kazeheki <kaminski.andrea@web.de>
2016-09-03 19:27:13 +0200

    Put links and link-methods into seperate file

--- js/alotoc.js
+++ js/alotoc.js
@@ -1,33 +1,3 @@
-var linkGit = "https://github.com/Kazeheki";
-var linkDocu = "docu/index.html";
-
-
-function goToHome(){
-  $(".content").fadeOut("slow", function(){
-    $(".contentInner").load("mainContent.html", function(){
-      $(".content").fadeIn("slow");
-    });
-  });
-
-  $(".menuBtn").removeClass("open");
-  $(".menuList").slideUp(600);
-};
-function goToGit(){
-  location.href = linkGit;
-};
-function goToScreenshots(){
-  $(".content").fadeOut("slow", function(){
-    $(".contentInner").load("screenshots.html", function(){
-      $(".content").fadeIn("slow");
-    });
-  });
-
-  $(".menuBtn").removeClass("open");
-  $(".menuList").slideUp(600);
-};
-function goToDocu(){
-  location.href = linkDocu;
-};
 
 var main = function(){
   $(".content").hide();
